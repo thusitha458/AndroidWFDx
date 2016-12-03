7f6e155c81c86d940a4426c8757260de9667eff2
thusitha458 <thusitha458@gmail.com>
2016-12-03 12:18:59 +0530

    Added experiments

---
+++ AndroidProjects/ManualAuth/WifiDirectTestApp/app/src/main/java/com/example/thusitha/wifidirecttestapp/experiments/PacketLossExperiment.java
@@ -0,0 +1,29 @@
+package com.example.thusitha.wifidirecttestapp.experiments;
+
+
+import com.example.thusitha.wifidirecttestapp.logging.FileLoggerFactory;
+import com.example.thusitha.wifidirecttestapp.logging.LoggerType;
+
+public class PacketLossExperiment extends PeriodicSender {
+
+    private static PacketLossExperiment instance = new PacketLossExperiment();
+
+    private PacketLossExperiment() {
+    }
+
+    public static PacketLossExperiment getInstance () {
+        return instance;
+    }
+
+    @Override
+    protected void setFileLogger () {
+        fileLogger = (new FileLoggerFactory()).getFileLogger(LoggerType.LOGGER_1);
+        fileLogger.createLogFile("pl-"
+                .concat(String.valueOf(periodMS).concat("-"))
+                .concat(String.valueOf(durationMS).concat("-"))
+                .concat(String.valueOf(distanceM))
+                .concat(".txt")
+        );
+    }
+
+}
